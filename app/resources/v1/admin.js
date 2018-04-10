f008d283d96a4d09626be66a262b3cb3fbc441e3
ankitkataria <ankitkataria28@gmail.com>
2018-04-10 18:11:43 +0530

    Adds admin authentication API

---
+++ app/resources/v1/admin.js
@@ -0,0 +1,36 @@
+var AdminUser = require('../../models/admin.js')
+var logger = require('../../../lib/logger.js')
+
+exports.post = function (req, res) {
+  var handleFindUser = function (err, user) {
+    if (err) {
+      logger.error(err)
+      res.status(500).send('Error finding an Admin user with the given email')
+      return
+    }
+
+    if (!user) {
+      res.status(404).send('An Admin user with the given email not found')
+    }
+
+    if (user.DOB === body.adminDOB && user.name === body.adminName) {
+      res.status(200).send('Admin user validated')
+    } else {
+      res.status(401).send('Incorrect credentials entered')
+    }
+  }
+
+  var body
+  try {
+    body = req.body
+  } catch (e) {
+    res.status(400).send('Could not parse body as JSON.')
+    return
+  }
+  console.log(body)
+  AdminUser.findOne({email: body.adminEmail}, handleFindUser)
+}
+
+exports.get = function (req, res) {
+  res.status(400).send('GET requests not allowed')
+}
